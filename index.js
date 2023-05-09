d03a06c17c315a172278e71e9fe5ef82900d39a1
Dharmandar <Dharmandar.kumar@dipoletechi.com>
2023-05-09 19:21:38 +0530

    added all dependancy file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { dialog } = require('electron')
-const ProgressBar = require('electron-progressbar');
+const { app,dialog,net,BrowserWindow } = require('electron')
 const { autoUpdater } = require('electron-updater');
-
+const cp = require('child_process')
+const fs = require('fs')
+const username = require('os').userInfo().username
+const path = require('path')
 
 class ElectronUpdater {
 	constructor(options = {}) {
@@ -39,8 +41,95 @@ class ElectronUpdater {
 	}
 }
 
+function splashWindow(mainWindow) {
+    splash = new BrowserWindow({
+        width: 600,
+        height: 400,
+        modal: true,
+        frame: true,
+        alwaysOnTop: false,
+        parent: mainWindow,
+        transparent: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
+    });
+
+    splash.loadFile(path.join(__dirname, './splash.html'));
+    splash.center();
+    mainWindow.hide()
+
+    splash.on('close', function () {
+        mainWindow.close()
+        app.quit()
+
+    })
+}
+
+function updateHandler(mainWindow) {
+
+    const request = net.request('http://localhost:8000/getversion')
+    request.on('response', (response) => {
+
+        console.log(`STATUS: ${response.statusCode}`)
+        console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
+        response.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`)
+            if (chunk > app.getVersion()) {
+                const dialogOpts = {
+                    type: 'question',
+                    title: 'Update ready to be installed',
+                    message: 'A new update is ready. When would you like to install?',
+                    buttons: ['Yes', 'Skip'],
+                    defaultId: 0,
+                    cancelId: 1,
+                };
+                dialog.showMessageBox(dialogOpts).then((returnValue) => {
+                    if (returnValue.response === 0) {
+                        splashWindow(mainWindow)
+                        callAutoUpdater();
+                    }
+                })
+            }
+        })
+        response.on('end', () => {
+            console.log('No more data in response.')
+        })
+    })
+    request.end();
+}
+
+function callAutoUpdater() {
+    const requestDownloadUpdate = net.request('http://localhost:8000/downloadupdate');
+    requestDownloadUpdate.on('response', (response) => {
+        console.log("donwloadUpdate", response);
+        const autoUpdate = new ElectronUpdater();
+        autoUpdate.checkForUpdates();
+    });
+    requestDownloadUpdate.end();
+};
+
+const updateInitiator = (mainWindow)=>{
+	let dirPath= 'C:/Users/' + username + '/AppData/ElectronUpdate';
+	fs.rmSync(dirPath, { force: true, recursive: true });
+
+        fs.mkdir(dirPath, (err) => {
+            if (err) {
+                console.log(err)
+                return;
+            }
+            cp.fork(path.resolve(__dirname, './server.js'))
+            updateHandler(mainWindow);
+        });
+};
+
+
 module.exports = {
 	ElectronUpdater,
+	updateHandler,
+	updateInitiator,
 	autoUpdate: options => {
 		const autoAppUpdater = new ElectronUpdater(options);
 		autoAppUpdater.checkForUpdates();
